486c63c688633c2334308880ceff6c422907b70f
GermanRodrickson <germanrodrickson@gmail.com>
2019-04-17 18:03:55 +0200

    add css

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+
+import Cabify from '../images/cabify.png';
 
 const Card = styled.div`
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
@@ -7,19 +9,41 @@ const Card = styled.div`
   height: 300px;
   display: inline-block;
   margin: 10px;
+  padding: 10px;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  display: block;
+  margin: 50px auto;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  display: block;
+  margin: 0 auto;
+  padding: 10px;
+  background-color: transparent;
+  font-size: 18px;
+`;
+
+const Title = styled.h3`
+  text-align: center;
+  font-size: 22px;
 `;
 
 const Item = props => {
   return (
     <Card>
-      {props.title}
-      <button
+      <Title>{props.title}</Title>
+      <Image src={Cabify} />
+      <Button
         onClick={() => {
           props.addToCart(props.title, props.item);
         }}
       >
-        +
-      </button>
+        Add to cart
+      </Button>
     </Card>
   );
 };
